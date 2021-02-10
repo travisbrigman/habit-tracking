184c3cc8efa327c823f383bfd483e14dad1982f0
travisbrigman <travis1000@gmail.com>
2021-02-10 09:30:33 -0600

    Initial Commit

---
+++ habit-tracking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  habit-tracking
+//
+//  Created by Travis Brigman on 2/10/21.
+//  Copyright © 2021 Travis Brigman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
